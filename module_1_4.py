69e4652a3dcdb8b38a0b7358eba50e65a14d9fa6
slike29 <craftcubs123@gmail.com>
2024-11-15 15:58:04 +1000

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+# Организация программы
+
+my_string = input()
+print(len(my_string))
+
+# Методы строк
+
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
